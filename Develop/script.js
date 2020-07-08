f1b51d615a96dabefdaf9b54b3f81fb7dfc6f79b
George F <64555318+GAFelton@users.noreply.github.com>
2020-07-08 13:40:48 -0700

    Pseudocode for timeblock builder.

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,28 @@
 $(document).ready(function() {
+    var pageHeader = $(".display-3");
+    var subTitle = $(".lead");
+    var currentDay = $("#currentDay");
+    var dayContainer = $("#dayContainer");
+    var todayIs = moment().format('dddd, MMMM Do YYYY, h:mm:ss a');
+    var hours = ["9:00", "10:00", "11:00", "12:00", "1:00", "2:00", "3:00", "4:00", "5:00"]
 
 
+    currentDay.text(todayIs);
+    // Pull events from localStorage.
 
 
+    //TIMEBLOCK BUILDER
+    // for loop through hours.length
+
+        //create div, assign class .row, attr("thisHour", hours[i]), id #row-(thisHour)
+        //create div, assign classes .col-md-2 .hour
+        //create div, assign classes .col-md-8 .time-bock
+        //create div, assign classes .col-md-2 .saveBtn
+
+        //append #row-(thisHour) to dayContainer sequentially.
+        //append .hour to #row-(thisHour)
+        //append .time-block to #row-(thisHour)
+        //append .saveBtn to #row-(thisHour)
 
 
 
