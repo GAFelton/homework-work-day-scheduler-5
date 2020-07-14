daf25522b6a6624ef87c336190e42f5c189e768e
George F <64555318+GAFelton@users.noreply.github.com>
2020-07-13 17:14:17 -0700

    First attempt at localStorage.

--- Develop/script.js
+++ Develop/script.js
@@ -5,73 +5,123 @@ $(document).ready(function () {
     var dayContainer = $("#dayContainer");
     var todayIs = moment().format('dddd, MMMM Do YYYY');
     var hours = ["9:00", "10:00", "11:00", "12:00", "1:00", "2:00", "3:00", "4:00", "5:00"]
+    var allNotes = [];
     var currentHour = moment().format("h");
-    var hoursOnly = hours.map(function(value){
+    var hoursOnly = hours.map(function (value) {
         var n = value.replace(":00", "");
         return n;
     });
     console.log(currentHour);
     console.log(hoursOnly);
-    var hoursIndex = hoursOnly.findIndex(function(value, index){
-        if (value === currentHour){
-        return index;
+    var hoursIndex = hoursOnly.findIndex(function (value, index) {
+        if (value === currentHour) {
+            return index;
         };
     });
     console.log(hoursIndex);
-    console.log(moment().format("h") + " is the time, with a type of: " +  typeof(moment().format("h")));
+    console.log(moment().format("h") + " is the time, with a type of: " + typeof (moment().format("h")));
 
 
     currentDay.text(todayIs);
     // Pull events from localStorage.
+   
+   
+    // function initLocalStorage() {
+    //     var storedText = JSON.parse(localStorage.getItem("allNotes"));
 
+    //     if (storedText !== null) {
+    //         allNotes = storedText;
+    //     }
+    // }
+
+    // function storeNote() {
+    //     var dataAttr = this.dataSaveBtn;
+    //     var note = "data-timeBlock", dataAttr;
+
+    //     var noteContent = {
+    //         "dataIndex": dataAttr,
+    //         "note": note
+    //     }
+
+    //     allNotes.push(noteContent);
+    //     localStorage.setItem("allNotes", JSON.stringify(allNotes));
+    // }
+    /*
+    To store in localStorage:
+    save button creates a new object to add to the allNotes array. Object has parameters dataIndex and note.
+    
+    What if each saveBtn added its own object to localStorage?
+    var dataAttr = this.attr("data-Savebtn");
+    var thisnote = {
+        "dataIndex": dataAttr,
+        "note": timeBlockDiv.textContent
+    }
+
+    localStorage.setItem("note-"+ dataAttr, JSON.stringify(thisnote))
+
+    When calling, forloop.
+    for i = 0; i<hours.length; i++) {
+        JSON.parse(localStorage.getItem("note-" + i));
+    }
+    */
 
     //TIMEBLOCK BUILDER
     // for loop through hours.length
-    for (var i = 0; i < hours.length; i++) {
-        //create div, assign class .row, attr("thisHour", hours[i])
-        var rowDiv = $("<div>");
-        rowDiv.addClass("row")
-        rowDiv.attr("data-thisHour", hours[i]);
+    function initSchedule() {
+        for (var i = 0; i < hours.length; i++) {
+            //create div, assign class .row, attr("thisHour", hours[i])
+            var rowDiv = $("<div>");
+            rowDiv.addClass("row")
+            rowDiv.attr("data-thisHour", hours[i]);
 
-        //create div, assign classes .col-md-1 .hour
-        var hourDiv = $("<div>");
-        hourDiv.addClass("col-md-1 hour float-left");
-        hourDiv.text(hours[i]);
-        //create div, assign classes .col-md-10 .time-bock
-        var timeBlockDiv = $("<div>");
-        timeBlockDiv.addClass("col-md-10 time-block");
+            //create div, assign classes .col-md-1 .hour
+            var hourDiv = $("<div>");
+            hourDiv.addClass("col-md-1 hour float-left");
+            hourDiv.text(hours[i]);
+            //create div, assign classes .col-md-10 .time-bock
+            var timeBlockDiv = $("<div>");
+            timeBlockDiv.addClass("col-md-10 time-block");
+            timeBlockDiv.attr("data-timeBlock", i);
 
-        //create div, assign classes .col-md-1 .saveBtn
-        var saveBtnDiv = $("<div>");
-        saveBtnDiv.addClass("col-md-1 saveBtn float-right far fa-save fa-3x");
+            //create div, assign classes .col-md-1 .saveBtn
+            var saveBtnDiv = $("<div>");
+            saveBtnDiv.addClass("col-md-1 saveBtn float-right far fa-save fa-3x");
+            saveBtnDiv.attr("dataSaveBtn", i);
+            saveBtnDiv.click(storeNote());
 
-        //append #row-(thisHour) to dayContainer sequentially.
-        dayContainer.append(rowDiv);
-        //append .hour to #row-(thisHour)
-        rowDiv.append(hourDiv);
-        //append .time-block to #row-(thisHour)
-        rowDiv.append(timeBlockDiv);
-        //append .saveBtn to #row-(thisHour)
-        rowDiv.append(saveBtnDiv);
+            // if (allNotes.find(function () {
+            //     i === dataIndex;
+            // })) {
+            //     timeBlockDiv.text(allNotes.noteContent);
+            // }
 
-        // console.log ( date & type)
+            //append #row-(thisHour) to dayContainer sequentially.
+            dayContainer.append(rowDiv);
+            //append .hour to #row-(thisHour)
+            rowDiv.append(hourDiv);
+            //append .time-block to #row-(thisHour)
+            rowDiv.append(timeBlockDiv);
+            //append .saveBtn to #row-(thisHour)
+            rowDiv.append(saveBtnDiv);
 
-        // parseInt() on hour before doing any mathematical comparison.
-        //time can be returned in MANY different formats. AMPM, military, time zones.
-        //Try using ASCII values. ASCII table for numbers.
 
-        if (i < hoursIndex) {
-            timeBlockDiv.addClass("past");
-        }
-        if (i === hoursIndex) {
-            timeBlockDiv.addClass("present");
-        }
-        if (i > hoursIndex) {
-            timeBlockDiv.addClass("future");
+
+            if (i < hoursIndex) {
+                timeBlockDiv.addClass("past");
+            }
+            if (i === hoursIndex) {
+                timeBlockDiv.addClass("present");
+            }
+            if (i > hoursIndex) {
+                timeBlockDiv.addClass("future");
+            }
+
+
         }
     }
 
-
+    initLocalStorage();
+    initSchedule();
 
 
 
@@ -94,3 +144,10 @@ Click on timeblock:
     Save button to save changes to localStorage.
 
 */
+
+// Notes for comparing time.
+// console.log ( date & type)
+
+// parseInt() on hour before doing any mathematical comparison.
+//time can be returned in MANY different formats. AMPM, military, time zones.
+//Try using ASCII values. ASCII table for numbers.
